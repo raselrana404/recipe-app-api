5059ab8ea92c221710387118d7dbb16b7702aa5d
Rasel Rana <rasel.rana.nsu@gmail.com>
2024-06-09 16:45:22 +0600

    Customization of admin pages for user list, user create and user edit.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # our apps
     'core',
+    'user',
+
+    # third party apps
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
